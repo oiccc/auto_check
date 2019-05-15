98614034eeb408a708ed5dcffbdd0890f349c4ed
oiccc <cimer_oic@163.com>
2019-05-15 17:31:51 +0800

    Update do.py

--- do.py
+++ do.py
@@ -30,8 +30,7 @@ def login_refresh_ression(login_username,login_password):
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36',
         'Upgrade-Insecure-Requests':'1'
         }
-
-    headersss_admin222 = {
+	headersss_admin222 = {
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'Accept-Encoding':'gzip, deflate, br',
         'Accept-Language':'zh-CN,zh;q=0.9',
